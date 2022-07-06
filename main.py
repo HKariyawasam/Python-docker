68f869d21e1c1e7270ea9345e3610d17edbb7505
UKARIKA <hasanikariyawasam.kariyawasamperadorapage@pearson.com>
2022-07-06 13:13:01 +0530

    updated python function to pass parameters

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Python program to print Even Numbers in given range
 
-start, end = 1, 100
+# start, end = 1, 100
 
 # iterating each number in list
-for num in range(start, end + 1):
+def print_range(start, end):
+    for num in range(start, end + 1):
 
-    # checking condition
-    if num % 2 == 0:
-        print(num, end=" ")
+        # checking condition
+        if num % 2 == 0:
+            print(num, end=" ")
