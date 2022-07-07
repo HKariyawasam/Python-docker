9b4a0a2eb517dbba57caaf00f8fa4e1b407b9313
UKARIKA <hasanikariyawasam.kariyawasamperadorapage@pearson.com>
2022-07-07 15:27:56 +0530

    updated aws job definition automation

--- create_job_queue.py
+++ create_job_queue.py
@@ -1,6 +1,8 @@
 import boto3
 
-client = boto3.client('batch')
+client = boto3.client('batch', aws_access_key_id='AKIAZKKSUAGURNDKXKKX',
+                      aws_secret_access_key='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',
+                      region_name='us-east-1')
 
 response = client.create_job_queue(
     jobQueueName='scripted_queue',
--- create_jobs.py
+++ create_jobs.py
@@ -1,6 +1,8 @@
 import boto3
 
-client = boto3.client('batch')
+client = boto3.client('batch', aws_access_key_id='AKIAZKKSUAGURNDKXKKX',
+                      aws_secret_access_key='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',
+                      region_name='us-east-1')
 
 response = client.submit_job(
     jobDefinition='scripted_job_definition_ecr3_v3',
